a820c8534ee58d21d798dcc624034ec00f974c51
NaitoYuki <naito.yuki127834@gmail.com>
2021-02-12 19:51:53 +0900

    update admin template

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,3 +1,7 @@
+crumb :root do
+  link "HOME", admin_root_path
+end
+
 crumb :admin_users do
   link t('admin.title.admin_users.index'), admin_admin_users_path
 end
@@ -7,9 +11,19 @@ crumb :admin_user do |admin_user|
   parent :admin_users
 end
 
-crumb :admin do |admin_user|
+crumb :new_admin_user do
+  link t('admin.actions.new'), new_admin_admin_user_path
+  parent :admin_users
+end
+
+crumb :edit_admin_user do |admin_user|
   link t('admin.actions.edit'), edit_admin_admin_user_path
   parent :admin_user, admin_user
+end
+
+crumb :password_admin_user do
+  link t('admin.header.password'), password_admin_admin_users_path
+  parent :admin_users
 end
 
 crumb :users do
